ac353c051cf94bfceb3e2c194dfad62823963f6a
Ondřej Mottl <ondrej.mottl@gmail.com>
2025-09-05 22:06:06 +0200

    Excercises: add DS exercises with the dynamic theme selection (own _quarto.yml file) and render script

---
+++ R/Exercises/render_excercises.R
@@ -0,0 +1,31 @@
+#----------------------------------------------------------#
+#
+#
+#               Reproducibility in Science
+#
+#                  Render Exercises
+#
+#
+#
+#                       O. Mottl
+#                         2025
+#
+#----------------------------------------------------------#
+
+# Setup -----
+
+library(here)
+
+source(
+  here::here("R/00_Config_file.R")
+)
+
+# Render -----
+quarto::quarto_render(
+  input = here::here("R/Exercises/01_tidyverse.qmd")
+)
+
+quarto::quarto_render(
+  input = here::here("R/Exercises/02_immutability.qmd")
+)
+ 
